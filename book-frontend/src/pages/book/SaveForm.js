748f1f30cdbef5ad3b6664466951b8ebf6075ea4
qkrtndh <qkrtndh5@naver.com>
2021-08-05 19:53:16 +0900

    상세보기

--- book-frontend/src/pages/book/SaveForm.js
+++ book-frontend/src/pages/book/SaveForm.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
-const SaveForm = () => {
+const SaveForm = (props) => {
+  const [book, setBook] = useState({
+    title: '',
+    author: '',
+  });
+
+  const changeValue = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitBook = (e) => {
+    //submit 동작을 막기위해서.
+    e.preventDefault();
+
+    fetch('http://localhost:8080/book', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      }, //전송할 데이터를 json으로 변경하여 던짐
+      body: JSON.stringify(book),
+    })
+      .then((res) => {
+        console.log(1, res);
+        if (res.status === 201) {
+          return res.json();
+        }
+        return null;
+      })
+      .then((res) => {
+        if (res !== null) {
+          props.history.push('/');
+        } else {
+          alert('등록실패');
+        }
+      });
+  };
   return (
-    <div>
-      <h1>책 등록</h1>
-    </div>
+    <Form onSubmit={submitBook}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter title"
+          onChange={changeValue}
+          name="title"
+        />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Author</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter Author"
+          onChange={changeValue}
+          name="author"
+        />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
