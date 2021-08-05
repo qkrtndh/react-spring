748b63677aa0e390f5a4b0acee5f4d9df25d30a0
qkrtndh <qkrtndh5@naver.com>
2021-08-05 17:44:49 +0900

    spring-react 글 목록 보기

--- book-frontend/src/pages/book/Home.js
+++ book-frontend/src/pages/book/Home.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Bookitem from '../../components/Bookitem';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  //함수 실행 시 최초 한번 실행
+  useEffect(() => {
+    //fetch는 데이터를 요청하는 javascript함수 기본적으로 get방식
+    //응답을 오면 json으로 받ㅇ므
+    fetch('http://localhost:8080/book')
+      .then((res) => res.json()) //처리될 데이터를 받는 것
+      .then((res) => {
+        //이후 실행할 함수
+        console.log(1, res);
+        setBooks(res);
+      }); //비동기함수
+  }, []); //한번만 실행되도록 빈 배열
+
   return (
     <div>
-      <h1>책 리스트 보기</h1>
+      {books.map((book) => (
+        <Bookitem key={book.id} book={book} />
+      ))}
     </div>
   );
 };
